d4a978a96889771b1ceccc1f13c631c600354d51
sanjay007 <yadavsanjay@hotmail.com>
2016-11-09 12:16:46 +0530

    mychanges

--- org.schoo.api/src/main/java/org/school/api/main/School_API.java
+++ org.schoo.api/src/main/java/org/school/api/main/School_API.java
@@ -10,7 +10,12 @@ import org.jsoup.select.Elements;
 import org.schoo.api.model.School;
 
 public class School_API {
-	 public static void main( String[] args) throws IOException
+	private static ArrayList<String> tobeVstd=new ArrayList<String>();
+	 
+	private static ArrayList<String> alreadyVstd=new ArrayList<String>();
+	private static String baseurl="http://www.studyguideindia.com/Schools/public-schools-india.html";
+	 
+	public static void main( String[] args) throws IOException
 	  {/*
 	   System.setProperty("http.proxySet", "true");
 	   System.setProperty("java.net.useSystemProxies", "true");
@@ -24,50 +29,73 @@ public class School_API {
 	   
 	   // prints HTML data
 	   */
-		 String baseurl="http://www.studyguideindia.com/Schools/public-schools-india.html";
 		 
-		 ArrayList<String> tobeVstd=new ArrayList<String>();
 		 
-		 ArrayList<String> alreadyVstd=new ArrayList<String>();
+		
 		 
-		 Document doc = Jsoup
+		/* Document doc = Jsoup
 			       .connect("http://www.studyguideindia.com/Schools/public-schools-india.html")
-			       .timeout(0).get();
+			       .timeout(0).get();*/
 		 
-		for( Element info : doc.select("div#ctl00_ContentPlaceHolder1_pagination")){
-			for(Element a :info.select("a[href]")){
-				tobeVstd.add(a.absUrl("href"));
+	
+		 Document doc = null;
+		 
+		 
+		if (tobeVstd.isEmpty()) {
+			doc = Jsoup
+					.connect(
+							baseurl)
+					.timeout(0).get();
+		} else {
+			while (!tobeVstd.isEmpty()) {
+				doc = Jsoup.connect(tobeVstd.get(0)).timeout(0).get();
+				alreadyVstd.add(tobeVstd.get(0));
 			}
-			
+
 		}
 		 
-		 
-	    //File input = new File("C://new.html");
-	    ArrayList<School> S = new ArrayList<School>();
-	    //Document doc = Jsoup.parse(input, "UTF-8");
-	    for(Element table : doc.select("table"))
-	    {
-	    	if(table.id().equals("ctl00_ContentPlaceHolder1_School_grid")){
-	      for(Element row : table.select("tr"))
-	      {
-	        Elements tds = row.select("td");
-	        School Sc = new School();
-	        if(!tds.isEmpty()){
-	        Sc.setName(tds.get(0).text());
-	        Sc.setType(tds.get(1).text());
-	        Sc.setDistrict(tds.get(2).text());
-	        Sc.setState(tds.get(3).text().toUpperCase());
-	        System.out.println(Sc.toString());
-	        S.add(Sc);
-	        }
+}
+	
+	 
+	 public static ArrayList<String> getLinksFromPage(Document doc){
+		
+		 for (Element info : doc
+					.select("div#ctl00_ContentPlaceHolder1_pagination")) {
+				for (Element a : info.select("a[href]")) {
+					tobeVstd.add(a.absUrl("href"));
+				}
+				
+				
+	 }
+		return tobeVstd; 
+	 }
 
-	      }
-	    	}
+	 public static ArrayList<School> getSchoolFromWeb(Document doc){
+			// File input = new File("C://new.html");
+			ArrayList<School> S = new ArrayList<School>();
+			// Document doc = Jsoup.parse(input, "UTF-8");
+			for (Element table : doc.select("table")) {
+				if (table.id().equals("ctl00_ContentPlaceHolder1_School_grid")) {
+					for (Element row : table.select("tr")) {
+						Elements tds = row.select("td");
+						School Sc = new School();
+						if (!tds.isEmpty()) {
+							Sc.setName(tds.get(0).text());
+							Sc.setType(tds.get(1).text());
+							Sc.setDistrict(tds.get(2).text());
+							Sc.setState(tds.get(3).text().toUpperCase());
+							System.out.println(Sc.toString());
+							S.add(Sc);
+						}
 
-	      System.out.println(getSqsl(S));
-	    }
-	  }
+					}
+				}
 
+				System.out.println(getSqsl(S));
+			}
+			return S;
+	 }
+	 
 	  public static String getSqsl( ArrayList<School> ins)
 	  {
 	    StringBuffer Sb = new StringBuffer();
